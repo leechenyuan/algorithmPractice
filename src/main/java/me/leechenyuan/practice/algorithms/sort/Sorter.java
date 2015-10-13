3f6d149ac084b134b717d4e6016bbf584e9a6cbb
handsomeCoder <463144199@qq.com>
2015-10-13 08:50:24 +0800

    clear;

--- src/main/java/me/leechenyuan/practice/algorithms/sort/Sorter.java
+++
@@ -1,8 +0,0 @@
-package me.leechenyuan.practice.algorithms.sort;
-
-/**
- * * Created by lee chenyuan on 2015/10/11.
- */
-public interface Sorter {
-    public void sort(int [] arr,int left,int right);
-}
